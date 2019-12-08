06c99d9e48fabcd23e572931dd1dd196c306661d
hiro-47 <13160306@g.matsuyama-u.ac.jp>
2019-12-08 19:10:43 +0900

    Initial Commit

---
+++ InstagramTake5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramTake5
+//
+//  Created by KATAOKA  HIROTAKA on 2019/12/08.
+//  Copyright © 2019 hirotaka.kataoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
