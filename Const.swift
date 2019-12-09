c653b80ca2ff8afd124a0a36021e07a9195a5abb
hiro-47 <13160306@g.matsuyama-u.ac.jp>
2019-12-09 11:43:15 +0900

    投稿画面追加

---
+++ Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  InstagramTake5
+//
+//  Created by KATAOKA  HIROTAKA on 2019/12/09.
+//  Copyright © 2019 hirotaka.kataoka. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
