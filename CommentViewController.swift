2dd68c83bd3bb0a6bbe803c521e179c3d1d586a9
hiro-47 <13160306@g.matsuyama-u.ac.jp>
2019-12-09 13:12:56 +0900

    一旦完成

---
+++ CommentViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CommentViewController.swift
+//  InstagramTake5
+//
+//  Created by KATAOKA  HIROTAKA on 2019/12/09.
+//  Copyright © 2019 hirotaka.kataoka. All rights reserved.
+//
+
+import UIKit
+
+class CommentViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
